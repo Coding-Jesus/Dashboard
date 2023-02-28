87d7b7a881286f3233c31519189f17552f6926ca
Coding-Jesus <blackjesusmp@gmail.com>
2023-02-28 14:30:44 +0000

    add Navbar design

--- dashboard/src/pages/home/Home.jsx
+++ dashboard/src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import './home.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
 const Home = () => {
     return (
         <div className='home'>
             <Sidebar />
-            <div className="homeContainer">homeContainer</div>
+            <div className="homeContainer">
+                <Navbar />
+                homeContainer</div>
         </div>
     )
 }
