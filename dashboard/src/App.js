85a5f4fb4113f581316edbc2039f904c75419045
Coding-Jesus <blackjesusmp@gmail.com>
2023-03-08 19:29:05 +0000

    add Formpage component

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -8,6 +8,7 @@ import List from "./pages/list/List";
 import Login from "./pages/login/Login";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
+import { productInputs, userInputs } from "./formSource";
 
 
 function App() {
@@ -23,14 +24,14 @@ function App() {
               <Route path=":userId" element={<Single />} />
               <Route
                 path="new"
-                element={<New title="Add New User" />}
+                element={<New inputs={userInputs} title="Add New User" />}
               />
             </Route>
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
               <Route
-                path="new" title="Add New Product" />
+                path="new" element={<New inputs={productInputs} title="Add New Products" />} />
             </Route>
           </Route>
         </Routes>
